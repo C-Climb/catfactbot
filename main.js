aafb141439bab5b6a403f7de47f2cca65f2de6db
C-Climb <deathmarchtepes@gmail.com>
2020-11-28 19:09:08 -0800

    Fixed dot env.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const Discord = require("discord.js");
 const client = new Discord.Client()
 const fetch = require("node-fetch")
